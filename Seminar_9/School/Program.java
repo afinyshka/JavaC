3c6f84584cf3a17e57fd616c9b76401f7a407d52
afinyshka <afinyshka@mail.ru>
2022-09-29 12:09:12 +0300

    add interface istanse of

--- Seminar_9/School/Program.java
+++ Seminar_9/School/Program.java
@@ -7,6 +7,9 @@ import Seminar_9.School.Classes.PrimarySchool;
 import Seminar_9.School.Classes.SchoolStudent;
 import Seminar_9.School.Classes.SecondarySchool;
 import Seminar_9.School.Classes.SeventhGradeSchool;
+import Seminar_9.School.Interface.IPlaying;
+import Seminar_9.School.Interface.ISkiping;
+import Seminar_9.School.Interface.ISmoking;
 
 public class Program {
     public static void main(String[] args) {
@@ -33,16 +36,17 @@ public class Program {
     public static void primarySchoolMethod(SchoolStudent student) {
         System.out.printf("   I'm a %s student:\n", student.getClass().getSimpleName());
         student.study();
-        if (student instanceof PrimarySchool) {
-            ((PrimarySchool) student).play();
-        } else if (student instanceof SecondarySchool) {
-            ((SecondarySchool) student).play();
-            ((SecondarySchool) student).skip();
-        } else if (student instanceof SeventhGradeSchool) {
-            ((SeventhGradeSchool) student).smoke();
-            ((SeventhGradeSchool) student).skip();
-        } else {
-            ((HighSchool) student).smoke();
+        if (student instanceof IPlaying) {
+            IPlaying playing = (IPlaying) student;
+            playing.play();
+        }
+        if (student instanceof ISmoking) {
+            ISmoking smoking = (ISmoking) student;
+            smoking.smoke();
+        }
+        if (student instanceof ISkiping) {
+            ISkiping skiping =  (ISkiping) student;
+            skiping.skip();
         }
     }
 }
