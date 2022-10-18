352162f910299aab428b9c8c382ea78fc9854740
afinyshka <afinyshka@mail.ru>
2022-10-18 13:04:01 +0300

    renew relations with logger

--- Seminar_11/HW_05/My001Calc/Presenter.java
+++ Seminar_11/HW_05/My001Calc/Presenter.java
@@ -11,16 +11,16 @@ import Seminar_11.HW_05.My001Calc.Interfaces.IView;
 public class Presenter {
     private final IView myView;
     private final IChoosingAction myModel;
-    private final ILogger logger;
+    private final ILogger mylogger;
     
 
-    public Presenter(IView myView, IChoosingAction myModel, ILogger logger) {
+    public Presenter(IView myView, IChoosingAction myModel, ILogger mylogger) {
         this.myView = myView;
         this.myModel = myModel;
-        this.logger = logger;
+        this.mylogger = mylogger;
     }
 
-    public void buttonClick(ILogger logger) throws IOException {
+    public void buttonClick() throws IOException {
         double a = myView.getValue("first number");
         String sign = myView.getSign("math char");
         double b = myView.getValue("second number");
@@ -30,6 +30,6 @@ public class Presenter {
         double result = myModel.chooseModel(sign, operandsList).result();
         String resString = String.format("%f %s %f = %f", a, sign, b, result);
         myView.showResult(resString);
-        logger.log(resString);
+        mylogger.log(resString);
     }
 }
--- Seminar_11/HW_05/My001Calc/Program.java
+++ Seminar_11/HW_05/My001Calc/Program.java
@@ -11,11 +11,11 @@ public class Program {
     public static void main(String[] args) throws IOException {
         ILogger logger = new SimpleLogger();
         Presenter pres = new Presenter (new View(), new ChoseModelFactory(), logger);
-        logger.getLog("my logger");
+        logger.getLog(Program.class.getSimpleName());
         logger.log("the program is started");
 
-        pres.buttonClick(logger);
-        pres.buttonClick(logger);
+        pres.buttonClick();
+        pres.buttonClick();
 
         logger.log("the program is finished");
     }
